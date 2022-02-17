6e2557361d7bc9b79d2738866d4ccbd14db9f8be
fahim-arif <fahim1.618555@gmail.com>
2022-02-17 19:11:04 +0600

    adding services and event bus

--- blog/event-bus/index.js
+++ blog/event-bus/index.js
@@ -5,15 +5,29 @@ const axios = require('axios');
 const app = express();
 app.use(express.json());
 
-app.post('/events', (req, res) => {
-    const event = req.body;
+const events = [];
 
-    axios.post('http://localhost:4000/events', event);
-    axios.post('http://localhost:4001/events', event);
-    axios.post('http://localhost:4002/events', event);
+app.post('/events', async (req, res) => {
+    try {
+        const event = req.body;
 
-    res.send({status:'OK'});
+        events.push(event);
+
+        await axios.post('http://localhost:4000/events', event);
+        await axios.post('http://localhost:4001/events', event);
+        await axios.post('http://localhost:4002/events', event);
+        await axios.post('http://localhost:4003/events', event);
+
+        res.send({status: 'OK'});
+    } catch (error) {
+
+        console.log(error)
+    }
 });
+
+app.get('/events', (req, res) => {
+    res.send(events);
+})
 
 app.listen(4005, () => {
     console.log('Listening on 4005');
